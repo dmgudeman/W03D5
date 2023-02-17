5f8d9e27d3f4d9c4040c5f7a1769a2777721d539
Alex Erdenberger <erdenberger.alexander@gmail.com>
2023-02-17 14:08:14 -0800

    Create PolyTreeNode initialize method and parent= method (incomplete)

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,14 @@
 class PolyTreeNode
-  
+
+    attr_reader :value, :children, :parent
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent=(node)
+        parent = node
+        parent.children << self 
+    end
 end
