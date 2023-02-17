d1ce2afa681dc14084683c328da3815f0c8ca451
Alex Erdenberger <erdenberger.alexander@gmail.com>
2023-02-17 14:53:20 -0800

    rename node variable in parent= to new node, create add_child and remove_child methods.

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -9,14 +9,23 @@ class PolyTreeNode
         @value = value
     end
 
-    def parent=(node)
+    def parent=(new_node)
         # debugger
-        return if parent == node
+        return if parent == new_node
         if self.parent
             self.parent.children.delete(self)
         end
 
-        @parent = node
+        @parent = new_node
         self.parent.children << self unless self.parent.nil?
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+    
+    def remove_child(child_node)
+        raise "I'm not a child!" if child_node.parent.nil?
+        child_node.parent = nil
     end
 end
